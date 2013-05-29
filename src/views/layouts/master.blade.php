68ef55af6cde183e5efc63f4a8230b6a89a92d20
David Bowen <david@lifeofabowen.com>
2013-05-29 21:49:12 +0100

    Added errors display for default layout

--- src/views/layouts/master.blade.php
+++ src/views/layouts/master.blade.php
@@ -3,6 +3,13 @@
     <meta charset="utf-8">
 </head>
 <body>
+    @if ($errors->any())
+        <h2>Errors</h2>
+        <ul>
+            {{ implode('', $errors->all('<li>:message</li>')) }}
+        </ul>
+    @endif
+
     @yield('body')
 </body>
 </html>
