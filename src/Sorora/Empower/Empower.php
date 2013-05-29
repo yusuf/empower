f94c947677d74a96cc6424afef45ca10a65b20dc
David Bowen <david@lifeofabowen.com>
2013-05-29 20:18:16 +0100

    Added helper functions to Empower

--- src/Sorora/Empower/Empower.php
+++ src/Sorora/Empower/Empower.php
@@ -2,5 +2,28 @@
 
 class Empower
 {
-    // public function 
+    public function form($type, $route, $extra = array())
+    {
+        return $this->$type($route, $extra);
+    }
+    private function store($route, $extra)
+    {
+        return \Form::open(
+            array(
+                'route' => $route.'.store'
+            )
+        );
+    }
+    private function update($route, $extra)
+    {
+        return \Form::model(
+            $extra['model'],
+            array(
+                'route' => array(
+                    $route.'.store',
+                    $extra['model']->id
+                )
+            )
+        );
+    }
 }
