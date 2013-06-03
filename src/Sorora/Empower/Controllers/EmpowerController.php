a3aca6019de31b638356c33d74bae6fe5431133b
David Bowen <david@lifeofabowen.com>
2013-06-03 16:20:05 +0100

    Added function to check for custom view

--- src/Sorora/Empower/Controllers/EmpowerController.php
+++ src/Sorora/Empower/Controllers/EmpowerController.php
@@ -11,4 +11,26 @@ class EmpowerController extends \BaseController {
             'baseurl' => $this->baseurl = \Config::get('empower::baseurl').'.'
         );
     }
+    /**
+     * Find out whether to use a custom view or not
+     *
+     * @param  string  $package
+     * @param  string  $folder
+     * @param  string  $view
+     * @return string
+     */
+    protected function viewFromConfig($package, $folder, $view)
+    {
+        $view_location = $folder.'.'.$view;
+        // Grab the configuration, if they are using custom view, it will be set to true
+        $config = \Config::get($package.'::views.'.$view_location);
+        // Base location for custom views
+        $package_folder = 'packages.sorora.'.$package.'.';
+        if(!$config) // Not using custom view
+        {
+            // Continue as normal package view
+            $package_folder = $package.'::';
+        }
+        return $package_folder.$view_location;
+    }
 }
