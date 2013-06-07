0535883064f392ca961832d4a1236d2ebea778e8
David Bowen <david@lifeofabowen.com>
2013-06-07 12:43:35 +0100

    Added EmpowerTestCase

---
+++ src/Sorora/Empower/Tests/EmpowerTestCase.php
@@ -0,0 +1,15 @@
+<?php namespace Sorora\Empower\Tests;
+
+class EmpowerTestCase extends \TestCase {
+
+    public function __call($method, $args)
+    {
+        if (in_array($method, ['get', 'post', 'put', 'patch', 'delete']))
+        {
+            return $this->call($method, $args[0]);
+        }
+
+        throw new BadMethodCallException;
+    }
+
+}
